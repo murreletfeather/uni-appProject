1ae5af957e5eafb13c995ead15008d37b5adb69f
murreletfeather <1941804585@qq.com>
2024-07-13 20:59:02 +0800

    聊天机器人，没改过来,失败了

--- myproject1/pages/index/firstPage/customerServiceRobot.vue
+++ myproject1/pages/index/firstPage/customerServiceRobot.vue
@@ -1,22 +1,245 @@
 <template>
 	<view>
-		
+		<view :animation="anData" style="height:0;"></view>
+		<scroll-view scroll-with-animation scroll-y="true" @touchmove="hideKey" style="width: 750rpx;"
+			:style="{'height': srcollHeight}" :scroll-top="go">
+			<view id="okk" scroll-with-animation>
+				<view class="flex-column-start" v-for="(x, i) in msgList" :key="i">
+					<view v-if="x.my" class="flex justify-end padding-right one-show  align-start  padding-top">
+						<view class="flex justify-end" style="width: 400rpx;">
+							<view class="margin-left padding-chat bg-cyan" style="border-radius: 35rpx;">
+								<text style="word-break: break-all;">{{ x.msg }}</text>
+							</view>
+						</view>
+					</view>
+					<view v-else class="flex-row-start margin-left margin-top one-show">
+						<view class="chat-img flex-row-center">
+							<image style="height: 75rpx; width: 75rpx;" src="@/static/image/robt.png" mode="aspectFit">
+							</image>
+						</view>
+						<view class="flex" style="width: 500rpx;">
+							<view class="margin-left padding-chat flex-column-start"
+								style="border-radius: 35rpx; background-color: #f9f9f9;">
+								<text style="word-break: break-all;">{{ x.msg }}</text>
+								<template v-if="x.type == 1">
+									<text style="color: #838383; font-size: 22rpx; margin-top: 15rpx;">你可以这样问我:</text>
+									<text @click="answer(index)" style="margin-top: 30rpx;"
+										v-for="(item, index) in x.questionList" :key="index">{{ item }}</text>
+								</template>
+								<template v-else-if="x.type == 2">
+									<text style="color: #838383; font-size: 22rpx; margin-top: 15rpx;">猜你想问:</text>
+									<text @click="answer(index)" style="margin-top: 30rpx;"
+										v-for="(item, index) in x.questionList" :key="index">{{ item }}</text>
+								</template>
+								<template v-else>
+									<text class="padding-top-sm" style="color: #2fa39b;">提交意见与反馈</text>
+									<text
+										style="color: #838383; font-size: 22rpx; margin-top: 15rpx;">下面是一些常见问题,您可以点击对应的文字快速获取答案:</text>
+									<text @click="answer(index)" style="margin-top: 30rpx; color: #2fa39b;"
+										v-for="(item, index) in x.questionList" :key="index">{{ item }}</text>
+								</template>
+							</view>
+						</view>
+					</view>
+				</view>
+				<view v-show="msgLoad" class="flex-row-start margin-left margin-top">
+					<view class="chat-img flex-row-center">
+						<image style="height: 75rpx; width: 75rpx;" src="@/static/image/robt.png" mode="aspectFit">
+						</image>
+					</view>
+					<view class="flex" style="width: 500rpx;">
+						<view class="margin-left padding-chat flex-column-start"
+							style="border-radius: 35rpx; background-color: #f9f9f9;">
+							<view class="cuIcon-loading turn-load" style="font-size: 35rpx; color: #3e9982;"></view>
+						</view>
+					</view>
+				</view>
+				<view style="height: 120rpx;"></view>
+			</view>
+		</scroll-view>
+
+		<view class="flex-column-center" style="position: fixed; bottom: -180px;" :animation="animationData">
+			<view class="bottom-dh-char flex-row-around" style="font-size: 55rpx;">
+				<input v-model="msg" class="dh-input" type="text" style="background-color: #f0f0f0;" @confirm="sendMsg"
+					confirm-type="search" placeholder-class="my-neirong-sm" placeholder="用一句简短的话描述您的问题" />
+				<view @click="sendMsg" class="cu-tag bg-cyan round">发送</view>
+				<text @click="ckAdd" class="cuIcon-roundaddfill text-brown"></text>
+			</view>
+			<view class="box-normal flex-row-around flex-wrap">
+				<view class="tb-text">
+					<view class="cuIcon-form"></view>
+					<text>问题反馈</text>
+				</view>
+				<view class="tb-text">
+					<view class="cuIcon-form"></view>
+					<text>人工客服</text>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
+	import {
+		onMounted,
+		reactive
+	} from 'vue';
+
 	export default {
-		data() {
+		setup() {
+			const state = reactive({
+				msgLoad: false,
+				anData: {},
+				animationData: {},
+				showTow: false,
+				msgList: [{
+					my: false,
+					msg: "你好我是客服机器人娜娜,请问有什么问题可以帮助您?(问候模板)",
+					type: 1,
+					questionList: ["如何注销用户", "我想了解业务流程", "手机号如何更换"]
+				}],
+				msg: "",
+				go: 0,
+				srcollHeight: 0
+			});
+
+			const sendMsg = () => {
+				if (state.msg === "") return;
+
+				state.msgList.push({
+					"msg": state.msg,
+					"my": true
+				});
+				msgGo();
+				msgKf(state.msg);
+				state.msg = "";
+			};
+
+			const msgGo = () => {
+				const query = uni.createSelectorQuery();
+				setTimeout(() => {
+					query.select('#okk').boundingClientRect(data => {
+						if (data.height - wh > 0) {
+							state.go = data.height - wh;
+						}
+						// ... (省略部分逻辑)
+					}).exec();
+				}, 100);
+			};
+
+			const msgKf = (x) => {
+				state.msgLoad = true;
+				setTimeout(() => {
+					state.msgLoad = false;
+					state.msgList.push({
+						my: false,
+						msg: "娜娜还在学习中,没能明白您的问题,请点击下方提交反馈与问题，我们会尽快人工处理。",
+						type: 0,
+						questionList: ["如何注销用户", "我想了解业务流程", "手机号如何更换"]
+					});
+					msgGo();
+				}, 2000);
+			};
+
+			const answer = (id) => {
+				console.log(id);
+			};
+
+			const hideKey = () => {
+				uni.hideKeyboard();
+			};
+
+			const ckAdd = () => {
+				if (!state.showTow) {
+					upTowmn(-180, 350);
+				} else {
+					upTowmn(0, 200);
+				}
+				state.showTow = !state.showTow;
+			};
+
+			const upTowmn = (x, t) => {
+				const animation = uni.createAnimation({
+					duration: t,
+					timingFunction: 'ease',
+				});
+				state.animation = animation;
+				animation.translateY(x).step();
+				state.animationData = animation.export();
+			};
+
+
+			onMounted(() => {
+				const query = getSystemInfoSync();
+				l.value = query.screenWidth / 750;
+				wh.value = query.windowHeight;
+				state.srcollHeight = `${query.windowHeight}px`;
+			});
 			return {
-				
-			}
-		},
-		methods: {
-			
+				...state,
+				sendMsg,
+				answer,
+				hideKey,
+				ckAdd,
+				upTowmn,
+			};
 		}
-	}
+	};
 </script>
 
 <style>
+	.bottom-dh-char {
+		background-color: #f9f9f9;
+		width: 750rpx;
+		height: 110rpx;
+	}
 
+	.center-box {
+		width: 720rpx;
+		padding-left: 25rpx;
+	}
+
+	.hui-box {
+		width: 750rpx;
+		height: 100%;
+	}
+
+	.dh-input {
+		width: 500rpx;
+		height: 65rpx;
+		border-radius: 30rpx;
+		padding-left: 15rpx;
+		background-color: #FFFFFF;
+	}
+
+	.box-normal {
+		width: 750rpx;
+		height: 180px;
+		background-color: #FFFFFF;
+	}
+
+	.tb-text view {
+		font-size: 65rpx;
+	}
+
+	.tb-text text {
+		font-size: 25rpx;
+		color: #737373;
+	}
+
+	.chat-img {
+		border-radius: 50%;
+		width: 100rpx;
+		height: 100rpx;
+		background-color: #f7f7f7;
+	}
+
+	.padding-chat {
+		padding: 17rpx 20rpx;
+	}
+
+	.tb-nv {
+		width: 50rpx;
+		height: 50rpx;
+	}
 </style>
--- myproject1/unpackage/dist/dev/mp-weixin/pages/index/firstPage/customerServiceRobot.js
+++ myproject1/unpackage/dist/dev/mp-weixin/pages/index/firstPage/customerServiceRobot.js
@@ -1,13 +1,150 @@
 "use strict";
 const common_vendor = require("../../../common/vendor.js");
+const common_assets = require("../../../common/assets.js");
 const _sfc_main = {
-  data() {
-    return {};
-  },
-  methods: {}
+  setup() {
+    const state = common_vendor.reactive({
+      msgLoad: false,
+      anData: {},
+      animationData: {},
+      showTow: false,
+      msgList: [{
+        my: false,
+        msg: "你好我是客服机器人娜娜,请问有什么问题可以帮助您?(问候模板)",
+        type: 1,
+        questionList: ["如何注销用户", "我想了解业务流程", "手机号如何更换"]
+      }],
+      msg: "",
+      go: 0,
+      srcollHeight: 0
+    });
+    const sendMsg = () => {
+      if (state.msg === "")
+        return;
+      state.msgList.push({
+        "msg": state.msg,
+        "my": true
+      });
+      msgGo();
+      msgKf(state.msg);
+      state.msg = "";
+    };
+    const msgGo = () => {
+      const query = common_vendor.index.createSelectorQuery();
+      setTimeout(() => {
+        query.select("#okk").boundingClientRect((data) => {
+          if (data.height - wh > 0) {
+            state.go = data.height - wh;
+          }
+        }).exec();
+      }, 100);
+    };
+    const msgKf = (x) => {
+      state.msgLoad = true;
+      setTimeout(() => {
+        state.msgLoad = false;
+        state.msgList.push({
+          my: false,
+          msg: "娜娜还在学习中,没能明白您的问题,请点击下方提交反馈与问题，我们会尽快人工处理。",
+          type: 0,
+          questionList: ["如何注销用户", "我想了解业务流程", "手机号如何更换"]
+        });
+        msgGo();
+      }, 2e3);
+    };
+    const answer = (id) => {
+      console.log(id);
+    };
+    const hideKey = () => {
+      common_vendor.index.hideKeyboard();
+    };
+    const ckAdd = () => {
+      if (!state.showTow) {
+        upTowmn(-180, 350);
+      } else {
+        upTowmn(0, 200);
+      }
+      state.showTow = !state.showTow;
+    };
+    const upTowmn = (x, t) => {
+      const animation = common_vendor.index.createAnimation({
+        duration: t,
+        timingFunction: "ease"
+      });
+      state.animation = animation;
+      animation.translateY(x).step();
+      state.animationData = animation.export();
+    };
+    common_vendor.onMounted(() => {
+      const query = getSystemInfoSync();
+      l.value = query.screenWidth / 750;
+      wh.value = query.windowHeight;
+      state.srcollHeight = `${query.windowHeight}px`;
+    });
+    return {
+      ...state,
+      sendMsg,
+      answer,
+      hideKey,
+      ckAdd,
+      upTowmn
+    };
+  }
 };
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
-  return {};
+  return {
+    a: _ctx.anData,
+    b: common_vendor.f(_ctx.msgList, (x, i, i0) => {
+      return common_vendor.e({
+        a: x.my
+      }, x.my ? {
+        b: common_vendor.t(x.msg)
+      } : common_vendor.e({
+        c: common_assets._imports_0,
+        d: common_vendor.t(x.msg),
+        e: x.type == 1
+      }, x.type == 1 ? {
+        f: common_vendor.f(x.questionList, (item, index, i1) => {
+          return {
+            a: common_vendor.t(item),
+            b: common_vendor.o(($event) => $setup.answer(index), index),
+            c: index
+          };
+        })
+      } : x.type == 2 ? {
+        h: common_vendor.f(x.questionList, (item, index, i1) => {
+          return {
+            a: common_vendor.t(item),
+            b: common_vendor.o(($event) => $setup.answer(index), index),
+            c: index
+          };
+        })
+      } : {
+        i: common_vendor.f(x.questionList, (item, index, i1) => {
+          return {
+            a: common_vendor.t(item),
+            b: common_vendor.o(($event) => $setup.answer(index), index),
+            c: index
+          };
+        })
+      }, {
+        g: x.type == 2
+      }), {
+        j: i
+      });
+    }),
+    c: common_assets._imports_0,
+    d: _ctx.msgLoad,
+    e: common_vendor.o((...args) => $setup.hideKey && $setup.hideKey(...args)),
+    f: _ctx.srcollHeight,
+    g: _ctx.go,
+    h: common_vendor.o((...args) => $setup.sendMsg && $setup.sendMsg(...args)),
+    i: _ctx.msg,
+    j: common_vendor.o(($event) => _ctx.msg = $event.detail.value),
+    k: common_vendor.o((...args) => $setup.sendMsg && $setup.sendMsg(...args)),
+    l: common_vendor.o((...args) => $setup.ckAdd && $setup.ckAdd(...args)),
+    m: _ctx.animationData
+  };
 }
 const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render]]);
 wx.createPage(MiniProgramPage);
